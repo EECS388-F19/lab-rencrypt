854992b45b6f76b2c2f10718ac7c865ecef37290
rahulani <rahulani@umich.edu>
2019-09-09 15:37:51 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+from random import randint
+a = randint(1, 100)
+b = randint(1, 100)
+sum = a + b
+avg = sum/2.0
+
+print("Rahul")
+print(a)
+print(b)
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
+
